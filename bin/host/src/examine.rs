604daf5eeacb2ac49df0980ca99df7a44a6fe0d7
Yuwen Zhang <yuwen01@gmail.com>
2025-05-01 13:54:44 -0700

    save before state too

--- bin/host/src/examine.rs
+++ bin/host/src/examine.rs
@@ -10,6 +10,33 @@ const DEBUG_STATE_BYTES: &[u8] = include_bytes!("../../../debug_state.bin");
 async fn main() -> eyre::Result<()> {
     let problem_address =
         hex_literal::hex!("0x00696ae3f9d462900e6af1d06d5ce49e0095874a086286234229b60eee11a37d");
+
+    let initial_big_state_bytes = std::fs::read("initial_big_state.bin").unwrap();
+    let initial_big_state =
+        bincode::deserialize::<EthereumState>(&initial_big_state_bytes).unwrap();
+
+    let initial_debug_state_bytes = std::fs::read("initial_debug_state.bin").unwrap();
+    let initial_debug_state =
+        bincode::deserialize::<EthereumState>(&initial_debug_state_bytes).unwrap();
+
+    println!(
+        "initial big state: {:?}",
+        initial_big_state.state_trie.get_rlp::<TrieAccount>(&problem_address)
+    );
+    println!(
+        "initial big state storage trie: {:?}",
+        initial_big_state.storage_tries.get(&problem_address).unwrap()
+    );
+
+    println!(
+        "initial debug state: {:?}",
+        initial_debug_state.state_trie.get_rlp::<TrieAccount>(&problem_address)
+    );
+    println!(
+        "initial debug state storage trie: {:?}",
+        initial_debug_state.storage_tries.get(&problem_address).unwrap()
+    );
+
     let big_state_bytes = std::fs::read("big_state.bin").unwrap();
     let big_state =
         rkyv::from_bytes::<EthereumState, rkyv::rancor::Error>(&big_state_bytes).unwrap();
